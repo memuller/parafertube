686bb950c7e18276e614e199765b8c96ca290548
Matheus E. Muller <hello@memuller.com>
2019-08-17 20:52:45 -0300

    chore(redux): adds planned action types

---
+++ src/store/actions.js
@@ -0,0 +1,5 @@
+export const SELECT_VIDEO = 'SELECT_VIDEO'
+export const SET_LOADING = 'SET_LOADING'
+export const SET_ERROR = 'SET_ERROR'
+export const SET_RESULTS = 'SET_RESULTS'
+export const FETCH_RESULTS = 'FETCH_RESULTS'
