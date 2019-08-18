ff54684f5f9140924c787c2070f9e3c56bf0e238
Matheus E. Muller <hello@memuller.com>
2019-08-17 23:05:14 -0300

    chore: refactors App as a class component

--- src/App.js
+++ src/App.js
@@ -3,34 +3,38 @@ import './App.scss';
 
 import { Row, Col, Container } from 'react-bootstrap'
 import MaterialIcon from 'material-icons-react'
+import { connect } from 'react-redux'
 
 import Search from './components/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="bg-dark py-2">
-        <Container>
-          <Row className="align-items-center">
-            <Col xs="12" md className="text-center text-md-left">
-                <h1 className="text-light">
-                  <MaterialIcon
-                    icon="video_library"
-                    size="medium" invert
-                  />
-                  <span>
-                    Parafertube
-                  </span>
-                </h1>
-            </Col>
-            <Col xs="12" md className="text-center text-md-right ">
-              <Search />
-            </Col>
-          </Row>
-        </Container>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <header className="bg-dark py-2">
+          <Container>
+            <Row className="align-items-center">
+              <Col xs="12" md className="text-center text-md-left">
+                  <h1 className="text-light">
+                    <MaterialIcon
+                      icon="video_library"
+                      size="medium" invert
+                    />
+                    <span>
+                      Parafertube
+                    </span>
+                  </h1>
+              </Col>
+              <Col xs="12" md className="text-center text-md-right ">
+                <Search />
+              </Col>
+            </Row>
+          </Container>
+        </header>
+        <article>
+        </article>
+      </div>
+    );
+  }
+}
