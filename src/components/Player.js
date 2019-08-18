b3ace302d5671dc88c0070172d9b6f83e38853fb
Matheus E. Muller <hello@memuller.com>
2019-08-18 11:44:24 -0300

    improv(player): shows player only when needed

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,14 +4,22 @@ import { ResponsiveEmbed } from 'react-bootstrap'
 import { connect } from 'react-redux'
 
 class Player extends React.Component {
-  
-  render(){
+
+
+  renderPlayer() {
     const videoURL = `https://www.youtube.com/embed/${this.props.videoId}?autoplay=1`
     return (
+      <ResponsiveEmbed aspectRatio="4by3">
+        <iframe title="player" src={videoURL} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </ResponsiveEmbed>
+    )
+  }
+
+  render(){
+    const content = this.props.videoId ? this.renderPlayer() : null
+    return (
       <div id="player">
-        <ResponsiveEmbed aspectRatio="4by3">
-          <iframe title="player" src={videoURL} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </ResponsiveEmbed>
+        {content}
       </div>
     )
   }
